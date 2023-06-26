d05f4dd6e9f3bbe858ed92f2af15bb0ba22608d6
gisa chris <gisachris24@gmail.com>
2023-06-26 17:23:00 -0200

    add navigation to webService

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/navigation';
+import NotFound from './components/notFound';
+import './styles/general.css';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/rockets" />
+        <Route path="/missions" />
+        <Route path="/profile" />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
