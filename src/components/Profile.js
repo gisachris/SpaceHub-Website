154107ab10c51e9f20cf20a953a15c857b3e733c
gisa chris <gisachris24@gmail.com>
2023-06-28 14:56:58 -0200

    Merge Rocket And Missions Profile View

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,33 @@
-/*eslint-disable*/
+import { useSelector } from 'react-redux';
 import DisplayMissions from './DisplayMissions';
+
 function Profile() {
+  const rocketData = useSelector((state) => state.rockets.Data);
+  const bookedRockets = rocketData.filter((rockets) => rockets.reserved === true);
   return (
-    <div className='profile'>
-      <DisplayMissions />
-      <div className='rockets'>
-        <h2>Rockets</h2>
-        <p>No rockets have been reserved yet!</p>
+    <div className="profile">
+      <div className="missions">
+        <DisplayMissions />
+      </div>
+      <div className="rockets">
+        <h2 className="rocketsHeader">Rockets</h2>
+        {bookedRockets.length === 0 && (
+          <p>No rockets have been reserved yet!</p>
+        )}
+
+        {bookedRockets.length > 0 && (
+          <table className="holderTable">
+            <tbody>
+              {bookedRockets.map((rocket) => (
+                <tr key={rocket.id} className="rocketrow">
+                  <td className="rocketdata">{rocket.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
 }
-
 export default Profile;
