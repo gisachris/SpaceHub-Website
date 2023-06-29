48205d345a6f845bac2cec3e834ed354bcccf99d
gisa chris <gisachris24@gmail.com>
2023-06-29 17:33:19 -0200

    Add Single Rocket Component

--- src/components/rocketView.js
+++ src/components/rocketView.js
@@ -1,6 +1,7 @@
 import '../styles/rockets.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { reserve, cancel } from '../redux/features/rocketsSlice';
+import Rocket from './rocket';
 
 const RocketsView = () => {
   const dispatch = useDispatch();
@@ -18,34 +19,12 @@ const RocketsView = () => {
 
   return (
     rocketsData.map((obj) => (
-      <div key={obj.id} className="rocketInfo">
-        <img src={obj.image} alt={`rocket ${obj.id}s display`} className="rocketImage" />
-        <section className="rocketSecInfo">
-          <span className="rocketTitle">{obj.name}</span>
-          {obj.reserved && (
-          <>
-            <div className="bookMarked">Reserved</div>
-            <p className="rocketParagraphbooked">{obj.description}</p>
-          </>
-          )}
-          {!obj.reserved && (
-          <p className="rocketParagraph">{obj.description}</p>
-          )}
-
-          {!obj.reserved && (
-          <button type="submit" className="bookRocket" onClick={() => handleReservations(obj.id)}>
-            Reserve Rocket
-          </button>
-          )}
-
-          {obj.reserved && (
-          <button type="submit" className="cancelRocket" onClick={() => handlecancelation(obj.id)}>
-            Cancel Reservation
-          </button>
-          )}
-
-        </section>
-      </div>
+      <Rocket
+        data={obj}
+        handleReservations={handleReservations}
+        handlecancelation={handlecancelation}
+        key={obj.id}
+      />
     ))
   );
 };
