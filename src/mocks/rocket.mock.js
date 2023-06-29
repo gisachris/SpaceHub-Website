aefe796f1b65eb21ccbcaab3e098c781de794539
gisa chris <gisachris24@gmail.com>
2023-06-29 09:44:35 -0200

    fix linter errors

--- src/mocks/rocket.mock.js
+++ src/mocks/rocket.mock.js
@@ -1,37 +1,36 @@
-import React from "react";
+/*eslint-disable*/
+import React from 'react';
 
-const RocketView = ({ rocketsData, handleReservations, handlecancelation }) => {
-  return (
-    rocketsData.map((obj) => (
-      <div key={obj.id} className="rocketInfo">
-        <img src={obj.image} alt={`rocket ${obj.id}'s display`} className="rocketImage" />
-        <section className="rocketSecInfo">
-          <span className="rocketTitle">{obj.name}</span>
-          {obj.reserved && (
-            <>
-              <div className="bookMarked">Reserved</div>
-              <p className="rocketParagraphbooked">{obj.description}</p>
-            </>
-          )}
-          {!obj.reserved && (
-            <p className="rocketParagraph">{obj.description}</p>
-          )}
+const RocketView = ({ rocketsData, handleReservations, handlecancelation }) => (
+  rocketsData.map((obj) => (
+    <div key={obj.id} className="rocketInfo">
+      <img src={obj.image} alt={`rocket ${obj.id}'s display`} className="rocketImage" />
+      <section className="rocketSecInfo">
+        <span className="rocketTitle">{obj.name}</span>
+        {obj.reserved && (
+        <>
+          <div className="bookMarked">Reserved</div>
+          <p className="rocketParagraphbooked">{obj.description}</p>
+        </>
+        )}
+        {!obj.reserved && (
+        <p className="rocketParagraph">{obj.description}</p>
+        )}
 
-          {!obj.reserved && (
-            <button type="submit" className="bookRocket" onClick={() => handleReservations(obj.id)}>
-              Reserve Rocket
-            </button>
-          )}
+        {!obj.reserved && (
+        <button type="submit" className="bookRocket" onClick={() => handleReservations(obj.id)}>
+          Reserve Rocket
+        </button>
+        )}
 
-          {obj.reserved && (
-            <button type="submit" className="cancelRocket" onClick={() => handlecancelation(obj.id)}>
-              Cancel Reservation
-            </button>
-          )}
-        </section>
-      </div>
-    ))
-  );
-}
+        {obj.reserved && (
+        <button type="submit" className="cancelRocket" onClick={() => handlecancelation(obj.id)}>
+          Cancel Reservation
+        </button>
+        )}
+      </section>
+    </div>
+  ))
+);
 
 export default RocketView;
--- src/mocks/rocketProfile.mock.js
+++ src/mocks/rocketProfile.mock.js
@@ -1,29 +1,39 @@
-import React from "react";
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const ProfileView = ({reservedData}) => {
-  return (
-    <section>
-      <div className="rockets">
-        <h2 className="rocketsHeader">Rockets</h2>
-        {reservedData.length === 0 && (
-          <p>No rockets have been reserved yet!</p>
-        )}
+const ProfileView = ({ reservedData }) => (
+  <section>
+    <div className="rockets">
+      <h2 className="rocketsHeader">Rockets</h2>
+      {reservedData.length === 0 && (
+      <p>No rockets have been reserved yet!</p>
+      )}
 
-        {reservedData.length > 0 && (
-          <table className="holderTable">
-            <tbody>
-              {reservedData.map((rocket) => (
-                <tr key={rocket.id} className="rocketrow">
-                  <td className="rocketdata">{rocket.name}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-    </section>
-  )  
+      {reservedData.length > 0 && (
+      <table className="holderTable">
+        <tbody>
+          {reservedData.map((rocket) => (
+            <tr key={rocket.id} className="rocketrow">
+              <td className="rocketdata">{rocket.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      )}
+    </div>
+  </section>
+);
 
-}
+ProfileView.propTypes = {
+  reservedData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      reserved: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default ProfileView;
